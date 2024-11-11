acd3cf4b8e8cd09f6a5025c4dd0e1b4eaa78a4ab
mvchemezov1 <144443468+mvchemezov1@users.noreply.github.com>
2024-11-11 18:45:40 +0500

    Delete Представление directory

--- Представление/Представление для всех медицинских услуг и цен.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW V_MedicalServices AS
-SELECT 
-    У.ID AS Услуга_ID,
-    У.Название_услуги AS Название,
-    У.Описание_услуги AS Описание,
-    У.Цена AS Цена
-FROM 
-    Медицинские_услуги У;
